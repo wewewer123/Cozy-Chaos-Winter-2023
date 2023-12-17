5fd243fdd743735fcf0e80185f7fd93e77951f5f
SplatTab <skidm44@gmail.com>
2023-12-17 13:52:36 -0600

    add death screen and add exit button

--- Assets/Scripts/Level_Manager.cs
+++ Assets/Scripts/Level_Manager.cs
@@ -1,32 +1,40 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 public class Level_Manager : MonoBehaviour
 {
+    private static Level_Manager _current;
+    public static Level_Manager current { get => _current; }
     public static int Current_Level = 0;
-    public static Level_Manager Instance;
-    private void Awake()
+    [SerializeField] GameObject deathMenu;
+    void Awake()
     {
-        //The singletone is created if there isnt one yet
-        if (Level_Manager.Instance == null)
+        if (_current == null)
         {
-            Level_Manager.Instance = this;
-            DontDestroyOnLoad(this.gameObject);
+            _current = this;
+            DontDestroyOnLoad(this.gameObject); // This will make the Level_Manager object persist between scenes
         }
         else
         {
-            Destroy(gameObject);
-
+            Debug.LogWarning("There shouldn't be more than one LevelManager in scene");
         }
     }
-
     static public void Level_Loader(int level)  //You can pass the index number of a level to load it
     {
         if (level > SceneManager.sceneCountInBuildSettings - 1) return; // Change this to load game over screen
         SceneManager.LoadSceneAsync(level);
         Current_Level = level;
     }
+    private IEnumerator DeathMenu()
+    {
+        deathMenu.SetActive(true);
+        yield return new WaitForSeconds(1.0f);
+        SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex);
+        yield return new WaitForSeconds(1.0f);
+        deathMenu.SetActive(false);
+    }
     static public void Reset_Level() //When called the scene manager reloads the current scene
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        _current.StartCoroutine(current.DeathMenu());
     }
 }
