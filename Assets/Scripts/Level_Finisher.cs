fc942de8a8e54eb1c17617177affef6dad87a567
wewewer123 <74647510+wewewer123@users.noreply.github.com>
2023-12-17 17:19:43 +0100

    catchup branch (#79)
    * Fixed player dying while still holding ball
    
    * Added Twinkle Shader
    
    * Add objective and fix some other things
    
    * FIx blue ball not removing scarf and color
    
    * Fix tilemap tearing
    
    * Add level 0
    
    * new level
    
    ---------
    
    Co-authored-by: Rakshat Kashyap <kashyaprakshat@gmail.com>
    Co-authored-by: Aiden Dupre <81938880+SplatTab@users.noreply.github.com>
    Co-authored-by: SplatTab <skidm44@gmail.com>

--- Assets/Scripts/Level_Finisher.cs
+++ Assets/Scripts/Level_Finisher.cs
@@ -2,10 +2,18 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class Level_Finisher : MonoBehaviour
 {
+    [SerializeField] Sprite CabinLit; // Reference to the lit cabin sprite
+    private SpriteRenderer sr;
+    private void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player")) // When the player enters the trigger calls the Level_Loader funciton in the game manager
         {
+            collision.gameObject.SetActive(false);
+            sr.sprite = CabinLit;
             Level_Manager.Level_Loader(SceneManager.GetActiveScene().buildIndex + 1);
         }
     }
